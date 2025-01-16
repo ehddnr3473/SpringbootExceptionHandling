e3735aa6fb52e04fbbe4426e07662301be883b27
김동욱 <ehddnr73@naver.com>
2025-01-16 16:36:37 +0900

    사용되지 않는 임포트 제거

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,7 +5,6 @@ import com.example.demo.service.HelloService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
